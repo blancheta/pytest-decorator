49e831628421a80b06a76448c3fef68d658288e9
aldazar <alexandreblanchet44@gmail.com>
2024-08-15 06:38:33 +0100

    fix: first commit

--- test_decorator.py
+++ test_decorator.py
@@ -2,13 +2,14 @@ from decorator import login_required
 
 
 def test_decorator(mocker):
-    mocker.patch(
+    session = mocker.patch(
         "decorator.session",
         {
             "oauth": 1,
             "supersecret": "abc"
         }
     )
+    print(session.get("supersecret"))
 
     is_valid_token_mock = mocker.patch("decorator.BBGithubAuth.is_valid_token")
     is_valid_token_mock.return_value = True
